8f40fa5f06e933a2d987254d81247ff561fcf306
upura <upura0@gmail.com>
2020-07-05 21:02:22 +0900

    add v feat

--- experiments/check.py
+++ experiments/check.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 cat = 'train'
 sid = '0000'
-X_seq = pd.read_csv(f'../input/{cat}/{sid}_feat.csv')
+X_seq = pd.read_csv(f'../input/{cat}/{sid}.csv')
 
-print(X_seq)
-print(X_seq.std(axis=0).values)
-print(X_seq / X_seq.std(axis=0).values)
+print(X_seq.columns)
+print(X_seq.head())
+
+print((X_seq['scr_x'].diff() ** 2 + X_seq['scr_y'].diff() ** 2) ** (1 / 2))
