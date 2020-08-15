4c11375d8f02799ccbc2504b2279f2c50acb0f50
upura <upura0@gmail.com>
2020-08-16 04:01:56 +0900

    w2 and w4

--- experiments/weighted_averaging.py
+++ experiments/weighted_averaging.py
@@ -56,9 +56,10 @@ def make_submission(pred, run_name: str, th: float):
 
 
 run_ids = [
-    'run006',
+    'weight002',
+    'weight004'
 ]
-run_name = 'weight004'
+run_name = 'weight005'
 
 y_train = Data.load('../input/y_train_fe000.pkl')
 data = [load_from_run_id(ri, to_rank=False) for ri in run_ids]
